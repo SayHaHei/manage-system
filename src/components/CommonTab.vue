37ce9405120635c0cf2452b6c7ed02ffa82cbd29
SayHaHei <1041711058@qq.com>
2020-02-19 20:45:31 +0800

    完成标签页面关闭跳转

--- src/components/CommonTab.vue
+++ src/components/CommonTab.vue
@@ -5,8 +5,9 @@
       size="small"
       v-for="(item, index) in tabList"
       :key="index"
-      @close="handleClose(item)"
+      @close="handleClose(item, index)"
       :effect="$route.name === item.name ? 'dark' : 'plain'"
+      @click="changeMenu(item)"
       >{{ item.label }}</el-tag
     >
   </div>
@@ -22,9 +23,24 @@ export default {
   },
   methods: {
     ...mapMutations(["closeTab"]),
-    handleClose(val) {
-      // this.$store.commit("closeTab", val);
+    handleClose(val, index) {
+      let length = this.tabList.length;
       this.closeTab(val);
+      if (val.name !== this.$route.name) {
+        return;
+      }
+      console.log("========", index, length);
+      /* 关闭的标签是最右边的化，向左移动一个 */
+      if (index === length) {
+        this.$router.push({ name: this.tabList[index - 1].name });
+      } else {
+        this.$router.push({ name: this.tabList[index].name });
+      }
+    },
+    changeMenu(item) {
+      this.$router.push({ name: item.name });
+      /* 点击时加入已点击列表 */
+      this.$store.commit("clickMenu", item);
     }
   }
 };
